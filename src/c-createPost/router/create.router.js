c7cf905fd3a37b9a548acb5852a8eff25f4515d2
gamezwar <gamezguerraalvaroandres@gmail.com>
2022-12-06 17:14:19 -0500

    create mongodb

--- src/c-createPost/router/create.router.js
+++ src/c-createPost/router/create.router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {Profile } = require('../../mongodb/models/Profile')
+
 //aqui manejo las rutas del create
 
 /**
@@ -10,26 +10,23 @@ const {Profile } = require('../../mongodb/models/Profile')
  * no requiere de parametro alguno 
  */
 
-// con esta ruta traigo todo los datos de Profile
-router.get('/', (req,res)=>{
 
-    Profile.find()
-           .then(data => res.status(200).json(data))
-           .catch(error => res.status(400).json({ message : error.message}))
+router.get('/',async (req,res)=>{
+
+    try {
+        
+        
+        res.status(200).json({
+            message:'Llegue!.',
+           
+        })
+
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+   
 })
 
-// creo los profile
-router.post('/',async (req, res)=>{
-    
-try {
-    const newProfile = await Profile.create(req.body)
-        res.status(200).json(newProfile);
-    
-} catch (error) {
-    res.status(400).send(error.message)
-}
-
-})
 
 
 
