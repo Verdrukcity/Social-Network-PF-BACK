b362e0f36bb8e5f5e8d38ef5b2601ff1e80f4030
gamezwar <gamezguerraalvaroandres@gmail.com>
2022-12-11 17:02:14 -0500

    Post

--- src/Post/services/Post.service.js
+++ src/Post/services/Post.service.js
@@ -1,4 +1,3 @@
-
 const { createImg, delImg } = require("../../cloudinary/index.js");
 const { Post } = require("../../mongodb/models/Post.js");
 const { Profile } = require("../../mongodb/models/Profile.js");
@@ -13,14 +12,14 @@ module.exports = {
             const { category, text, } = req.query;
             const{id} = req.body;
             if (category || text) {
-                const FIND_POSTS = await Post.find({ type, text, })
+                const FIND_POSTS = await Post.find({ type, text, }).populate(['userId', 'commentId'])
                 res.json(FIND_POSTS)
             } 
             if(id){
-                const POST = await Post.findById(id);
+                const POST = await Post.findById(id).populate(['userId', 'commentId'])
                 res.json(POST)
             } else{
-                const POSTS = await Post.find({})
+                const POSTS = await Post.find({}).populate(['userId', 'commentId'])
                 const INFO = []
                 for (const iterator of POSTS) {                             //use a for method, because .map doasen't work
                     const uId = mongoose.Types.ObjectId(iterator.userId);   //conver the id on something that mongoores recognice
@@ -87,15 +86,83 @@ module.exports = {
     },
     detailPost: async(req, res)=>{
         try{
-            //extraemos el id del post por params
-            const {postId} = req.params
-            //buscamos el post con el id requerido
-            const searchedPost = await Post.findById(postId)
+            // //extraemos el id del post por params
+            // const {postId} = req.params
+            // //buscamos el post con el id requerido
+            // const searchedPost = await Post.findById(postId)
 
-            //si es null, no existe tal post
-            if (searchedPost == null) throw new Error('No pudimos encontrar el post que buscas')
-            //de lo contrario, devolvemos el post obtenido
-            else res.json(searchedPost) 
+            // //si es null, no existe tal post
+            // if (searchedPost == null) throw new Error('No pudimos encontrar el post que buscas')
+            // //de lo contrario, devolvemos el post obtenido
+            // else res.json(searchedPost) 
+            const { postId } = req.params
+            const id = mongoose.Types.ObjectId(postId);
+            let p = await Post.aggregate([
+                {
+                $match : {"_id" : id}
+            },
+            {
+                $lookup : {
+                    from: "profiles",
+                    localField : 'userId',
+                    foreignField : '_id',
+                    as : 'profile'
+                }
+            },
+            {
+                        $graphLookup: {
+                            from: "comments",
+                            startWith: "$commentId",
+                            connectFromField: "_id",
+                            connectToField: "_id",
+                            maxDepth: 2,
+                            depthField: "numConnections",
+                            as: "comentarios"
+                        }
+                    },
+                    {
+                        $unwind : '$comentarios'
+                    },
+                    {
+                        $graphLookup: {
+                            from: "profiles",
+                            startWith: "$comentarios.profileId",
+                            connectFromField: "_id",
+                            connectToField: "_id",
+                            maxDepth: 2,
+                            depthField: "numConnections",
+                            as: "usuarios"
+                        }
+                    },
+                    {
+                        $unwind : '$usuarios'
+                    },
+                {$project : {userId:0, commentId : 0, profile : {content : 0}, usuarios : {content : 0}}}
+        ]);
+        if(p.length){
+            const arr = {
+                userId : p[0].profile,
+                post : {
+                    _id : p[0]._id,
+                    text : p[0].text,
+                    multimedia : p[0].multimedia,
+                    multimedia_id : p[0].multimedia_id,
+                    category : p[0].category
+                },
+                comment : p.map((x) =>{
+                    return{
+                        comment : x.comentarios && x.comentarios,
+                        user : x.usuarios && x.usuarios
+                    }
+                })    
+            }
+            return res.status(200).json(arr)
+        }
+    
+        if(!p.length){
+            p = await Post.findById(postId).populate(['userId', 'commentId']);
+               res.status(200).json(p)
+         } 
         }
         catch(error){
             res.status(400).send(error.message)
