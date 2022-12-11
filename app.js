9a99d50e6748cf87f89a73e20921d0a9357fa8c7
andres <andresflobolugo00@gmail.com>
2022-12-10 20:50:34 -0400

    para subir imagenes

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next(); 
 })
 app.use(express.static(path.join(__dirname, 'public')));
