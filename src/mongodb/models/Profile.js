560114b5615022237e69f8075fc5c4f17397d8df
unknown <rodrigarlati@gmail.com>
2022-12-15 15:52:17 -0300

    Mensaje personalizado de cual es el problema en las validaciones

--- src/mongodb/models/Profile.js
+++ src/mongodb/models/Profile.js
@@ -4,6 +4,8 @@ const { Follower } = require("./Follower");
 const { Mapa } = require("./mapas");
 const { Post } = require("./Post");
 const { validateEmail, validateAge } = require("./validations/index");
+var uniqueValidator = require('mongoose-unique-validator');
+
 
 const PROFILE = new Schema({
     email: {
@@ -11,7 +13,10 @@ const PROFILE = new Schema({
         required: function () {
             return validateEmail(this.email);
         },
-        unique: true,
+        unique: {
+            value: true,
+            message: 'Email repetido'
+        }
     },
     user_Name: {
         type: String,
@@ -65,6 +70,9 @@ const PROFILE = new Schema({
         },
     ],
 });
+
+//Mensaje personalizado para avisar cual es el problema
+PROFILE.plugin(uniqueValidator, {message: `El ${this.PATH} ingresado ya se encuentra en uso`});
 
 const Profile = model("Profile", PROFILE);
 module.exports = { Profile };
