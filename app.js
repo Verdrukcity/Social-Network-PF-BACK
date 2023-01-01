35678a60828f11f80c0ee9d7d8513debfc3cb234
Verdrukcity <motavercity@gmail.com>
2023-01-01 16:45:17 -0600

    deploy

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*')
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
