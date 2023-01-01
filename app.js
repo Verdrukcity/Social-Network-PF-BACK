ae04bdd1ce9f632306c54c66173c57d0e661a75b
Verdrukcity <motavercity@gmail.com>
2023-01-01 17:21:02 -0600

    deploy

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*')
-    res.header('Access-Control-Allow-Credentials',true);
-    res.header('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Methods','Content-Type','Authorization');
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Credentials',true);
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next(); 
 })
 app.use(express.static(path.join(__dirname, 'public')));
