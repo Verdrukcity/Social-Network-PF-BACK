3290a188fd5d1bc5a51947328c9b218c4976badf
gamezwar <gamezguerraalvaroandres@gmail.com>
2023-01-06 14:08:36 -0500

    admin

--- src/Admin/router/create.admin.js
+++ src/Admin/router/create.admin.js
@@ -1,21 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {
-    Message_Error_Create_Post
-} = require( "../../Message");
 const { upUser, postUp } = require('../services/Admin.service.js');
-
-//aqui manejo las rutas del create
 
 /**
  * trae todos los post o los seleccionados
- * tipo : get
+ * tipo : post
  * ruta : /admin
- * no requiere de parametro alguno pero puede ocupar Query
  */
 
-//                                      Search all || search by text || search by type
-router.get("/" , async (req, res) => {
+router.post("/" , async (req, res) => {
     try {
       await upUser(req, res)
     } catch (error) {
@@ -23,7 +16,7 @@ router.get("/" , async (req, res) => {
     }
 })
 
-router.get("/post" , async (req, res) => {
+router.post("/post" , async (req, res) => {
     try {
       await postUp(req, res)
     } catch (error) {
